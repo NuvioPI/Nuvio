982a66f277f534bdfce1e7bc9362e166b2c81f4d
yeMorGx <gabrielmcgoes@gmail.com>
2026-06-11 21:12:32 -0300

    feat: initialize public index.php with CORS headers and status endpoint

--- nuvio-back/public/index.php
+++ nuvio-back/public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-header("Access-Control-Allow-Origin: https://nuvioapp.vercel.app");
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Content-Type: application/json; charset=UTF-8");
